73fa3b7b814ccb92c2b53193f9f6644a3f9293b8
MikuroXina <ryosukadnak@gmail.com>
2023-11-20 13:25:26 +0900

    Format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: '出席確認システム - 教員用',
-  description: '講義の出席状況を確認、修正、提出しましょう。',
-}
+  title: "出席確認システム - 教員用",
+  description: "講義の出席状況を確認、修正、提出しましょう。",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ja">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
