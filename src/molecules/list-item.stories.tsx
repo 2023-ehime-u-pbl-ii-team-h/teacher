6ad8ce45949bd6ebdfdf657938482594ca022d05
MikuroXina <ryosukadnak@gmail.com>
2023-12-19 03:02:33 +0900

    Add story

---
+++ src/molecules/list-item.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { ListItem } from "./list-item";
+
+export default {
+  title: "molecules/top navigation bar",
+  component: ListItem,
+} satisfies Meta<typeof ListItem>;
+
+type Story = StoryObj<typeof ListItem>;
+
+export const Normal: Story = {
+  args: {
+    checked: false,
+    iconLabel: "TS",
+    primaryLabel: "TEST Student",
+    secondaryLabel: "test.student@example.com",
+  },
+};
+
+export const Checked: Story = {
+  args: {
+    checked: true,
+    iconLabel: "TS",
+    primaryLabel: "TEST Student",
+    secondaryLabel: "test.student@example.com",
+  },
+};
