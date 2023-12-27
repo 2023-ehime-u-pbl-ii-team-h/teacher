406a4546da614941120ab0428a7cb9108ba48148
h007akim <h520007u@mails.cc.ehime-u.ac.jp>
2023-12-27 12:11:17 +0900

    a

---
+++ src/molecules/top-navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./top-navbar.css";
+import { AiOutlineUnorderedList } from "react-icons/ai";
+
+const Navbar = () => {
+    return (
+        <div className={styles.navbar}>
+            <AiOutlineUnorderedList />
+            <>aa</>
+            <>bb</>
+        </div>
+    );
+};
+
+export default Navbar;
