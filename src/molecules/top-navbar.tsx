c89a8fdc7a4aa9d2988f305544ef94778a084b23
h007akim <h520007u@mails.cc.ehime-u.ac.jp>
2024-01-09 18:37:35 +0900

    Update navbar

--- src/molecules/top-navbar.tsx
+++ src/molecules/top-navbar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import styles from "./top-navbar.css";
-import { AiOutlineUnorderedList } from "react-icons/ai";
+import styles from "./top-navbar.module.css";
+import { AiOutlineUnorderedList, AiOutlineUser } from "react-icons/ai";
+
 
 const Navbar = () => {
     return (
         <div className={styles.navbar}>
             <span className={styles.icon}><AiOutlineUnorderedList /></span>
             <span className={styles.subjectname}>科目名</span>
-            <span className={styles.icon}>アバターアイコン</span>
+            <span className={styles.icon}><AiOutlineUser /></span>
         </div>
     );
 };
