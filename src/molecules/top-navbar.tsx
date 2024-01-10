932ed4b11f37eeaf053b84b086e49c3ab796e209
h007akim <h520007u@mails.cc.ehime-u.ac.jp>
2024-01-10 16:12:32 +0900

    accountmenu

--- src/molecules/top-navbar.tsx
+++ src/molecules/top-navbar.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./top-navbar.module.css";
 import { AiOutlineUnorderedList, AiOutlineUser } from "react-icons/ai";
 
-
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div className={styles.navbar}>
             <span className={styles.icon}><AiOutlineUnorderedList /></span>
             <span className={styles.subjectname}>科目名</span>
-            <span className={styles.icon}><AiOutlineUser /></span>
+            <span className={styles.icon}><AiOutlineUser onClick={toggleMenu} /></span>
+            {isMenuOpen && (
+                <div className="accountmenu">
+                    <ul>
+                        <li>menu item</li>
+                    </ul>
+                </div>
+            )}
         </div>
     );
 };
