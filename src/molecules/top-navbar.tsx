514970197c86afd20177fa7a86a1585b79d41272
MikuroXina <ryosukadnak@gmail.com>
2024-01-11 11:42:52 +0900

    Fix top nav bar style

--- src/molecules/top-navbar.tsx
+++ src/molecules/top-navbar.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import styles from "./top-navbar.module.css";
 import { AiOutlineUnorderedList, AiOutlineUser } from "react-icons/ai";
 
-
 const Navbar = () => {
-    return (
-        <div className={styles.navbar}>
-            <span className={styles.icon}><AiOutlineUnorderedList /></span>
-            <span className={styles.subjectname}>科目名</span>
-            <span className={styles.icon}><AiOutlineUser /></span>
-        </div>
-    );
+  return (
+    <div className={`${styles.navbar} surface on-surface-text`}>
+      <span className={styles.icon}>
+        <AiOutlineUnorderedList />
+      </span>
+      <span className={styles.subjectname}>科目名</span>
+      <span className={styles.icon}>
+        <AiOutlineUser />
+      </span>
+    </div>
+  );
 };
 
 export default Navbar;
