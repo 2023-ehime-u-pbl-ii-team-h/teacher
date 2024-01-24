7893e3d7e41120f06d5400c7f5a4137161f5f644
h007akim <h520007u@mails.cc.ehime-u.ac.jp>
2024-01-24 16:58:56 +0900

    Update top-navbar.tsx

--- src/molecules/top-navbar.tsx
+++ src/molecules/top-navbar.tsx
@@ -34,41 +34,74 @@ export const Navbar = ({ title }: NavbarProps) => {
     };
   }, []);
 
-  //if
-
-  return (
-    <>
-      <SideMenu
-        title={title}
-        isOpen={isOpenSideMenu}
-        onClose={() => setIsOpenSideMenu(false)}
-      />
-      <div className={`${styles.navbar} surface on-surface-text`}>
-        <StandardIconButton
-          icon={<AiOutlineUnorderedList />}
-          alt="サイドメニューを開く"
-          onClick={() => setIsOpenSideMenu(true)}
+  if (login.data) {
+    return (
+      <>
+        <SideMenu
+          title={title}
+          isOpen={isOpenSideMenu}
+          onClose={() => setIsOpenSideMenu(false)}
         />
-        <span className={styles.subjectName}>{title}</span>
-        <StandardIconButton
-          icon={<AiOutlineUser />}
-          alt=""
-          onClick={() => setIsOpenAccountMenu(true)}
+        <div className={`${styles.navbar} surface on-surface-text`}>
+          <StandardIconButton
+            icon={<AiOutlineUnorderedList />}
+            alt="サイドメニューを開く"
+            onClick={() => setIsOpenSideMenu(true)}
+          />
+          <span className={styles.subjectName}>{title}</span>
+          <StandardIconButton
+            icon={<AiOutlineUser />}
+            alt=""
+            onClick={() => setIsOpenAccountMenu(true)}
+          />
+          {isOpenAccountMenu && (
+            <Menu ref={menuRef}>
+              <MenuLabel>
+                <div>
+                  <div className="label-large">{login.data.name}</div>
+                  <div className="label-medium">{login.data.email}</div>
+                </div>
+              </MenuLabel>
+              <MenuButton>
+                <span>ログアウト</span>
+              </MenuButton>
+            </Menu>
+          )}
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <SideMenu
+          title={title}
+          isOpen={isOpenSideMenu}
+          onClose={() => setIsOpenSideMenu(false)}
         />
-        {isOpenAccountMenu && (
-          <Menu ref={menuRef}>
-            <MenuLabel>
-              <div>
-                <div className="label-large">TEST Teacher</div>
-                <div className="label-medium">{login.data.email}</div>
-              </div>
-            </MenuLabel>
-            <MenuButton>
-              <span>ログイン</span>
-            </MenuButton>
-          </Menu>
-        )}
-      </div>
-    </>
-  );
+        <div className={`${styles.navbar} surface on-surface-text`}>
+          <StandardIconButton
+            icon={<AiOutlineUnorderedList />}
+            alt="サイドメニューを開く"
+            onClick={() => setIsOpenSideMenu(true)}
+          />
+          <span className={styles.subjectName}>{title}</span>
+          <StandardIconButton
+            icon={<AiOutlineUser />}
+            alt=""
+            onClick={() => setIsOpenAccountMenu(true)}
+          />
+          {isOpenAccountMenu && (
+            <Menu ref={menuRef}>
+              <MenuLabel>
+                <div>ログインしていません</div>
+              </MenuLabel>
+              <MenuButton>
+                <span>ログイン</span>
+              </MenuButton>
+            </Menu>
+          )}
+        </div>
+      </>
+    );
+  }
 };
