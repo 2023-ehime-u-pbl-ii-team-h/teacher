602cb8a6956421b3f46ee2a1c7c95b7d5be8325d
MikuroXina <ryosukadnak@gmail.com>
2024-01-24 09:56:18 +0900

    Fix return type of useSubjects

--- src/queries/subjects.ts
+++ src/queries/subjects.ts
@@ -4,27 +4,31 @@ import { API_ROOT } from "./config";
 export interface Subject {
   id: string;
   name: string;
-  lastOpenDate: Date;
+  boards: {
+    id: string;
+    subject: string;
+    startFrom: string;
+    secondsFromStartToBeLate: number;
+    secondsFromBeLateToEnd: number;
+  }[];
 }
 
 const fetcher: Fetcher<Subject[], Record<string, never>> = () =>
-  fetch(`${API_ROOT}/me/subjects`)
-    .then(
-      (res) =>
-        res.json() as Promise<
-          {
+  fetch(`${API_ROOT}/me/subjects`).then(
+    (res) =>
+      res.json() as Promise<
+        {
+          id: string;
+          name: string;
+          boards: {
             id: string;
-            name: string;
-            lastDate: string;
-          }[]
-        >,
-    )
-    .then((values) =>
-      values.map((value) => ({
-        id: value.id,
-        name: value.name,
-        lastOpenDate: new Date(value.lastDate),
-      })),
-    );
+            subject: string;
+            startFrom: string;
+            secondsFromStartToBeLate: number;
+            secondsFromBeLateToEnd: number;
+          }[];
+        }[]
+      >,
+  );
 
 export const useSubjects = () => useSWR({}, fetcher);
