f5f0510ec22f3c346cb52fd57280b0722cc24738
MikuroXina <ryosukadnak@gmail.com>
2024-01-24 18:06:06 +0900

    Fix to require access token

--- src/commands/logout.ts
+++ src/commands/logout.ts
@@ -1,8 +1,11 @@
 const BACKEND_LOGOUT_ENDPOINT = "https://backend.mikuroxina.workers.dev/logout";
 
-export const logoutAndReload = async () => {
+export const logoutAndReload = async (accessToken: string) => {
   try {
     const response = await fetch(BACKEND_LOGOUT_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
       method: "POST",
     });
 
