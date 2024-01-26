98ec693f3943a70f4b8daa4fc8117ae5b8f3b239
MikuroXina <ryosukadnak@gmail.com>
2024-01-26 09:11:16 +0900

    Add putBoard

---
+++ src/commands/put-board.ts
@@ -0,0 +1,27 @@
+import { AttendanceBoards } from "@/queries/boards";
+import { API_ROOT } from "@/queries/config";
+
+export async function putBoard(
+  accessToken: string,
+  subjectId: string,
+  board: AttendanceBoards[number],
+) {
+  const res = await fetch(
+    `${API_ROOT}/subjects/${subjectId}/board/${board.id}`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        start_from: board.startFrom,
+        seconds_from_start_to_be_late: board.secondsFromStartToBeLate,
+        seconds_from_be_late_to_end: board.secondsFromBeLateToEnd,
+      }),
+    },
+  );
+  if (!res.ok) {
+    throw new Error(await res.text());
+  }
+}
