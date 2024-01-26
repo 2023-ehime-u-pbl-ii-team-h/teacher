f4e30f15eb30703bfd76eca1dd3d31c54265f72f
MikuroXina <ryosukadnak@gmail.com>
2024-01-26 09:32:40 +0900

    Implement onSubmit

--- src/organisms/new-board-form.tsx
+++ src/organisms/new-board-form.tsx
@@ -4,10 +4,46 @@ import { FilledButton } from "@/atoms/button";
 import { OutlinedTextField } from "@/atoms/text-field";
 import { FormEvent } from "react";
 import styles from "./new-board-form.module.css";
+import { postBoard } from "@/commands/post-board";
+import { useAccessToken } from "@/queries/access-token";
+import { useSearchParams, useRouter } from "next/navigation";
 
 export function NewBoardForm(): JSX.Element {
+  const accessToken = useAccessToken();
+  const params = useSearchParams();
+  const router = useRouter();
+
+  if (!params.has("subject")) {
+    router.replace("/");
+  }
+
   function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    if (!accessToken) {
+      return;
+    }
+
+    const startFrom = (
+      event.currentTarget.elements.namedItem("start_from") as HTMLInputElement
+    ).value;
+    const startToBeLate = (
+      event.currentTarget.elements.namedItem(
+        "between_start_and_be_late",
+      ) as HTMLInputElement
+    ).value;
+    const beLateToEnd = (
+      event.currentTarget.elements.namedItem(
+        "between_be_late_and_end",
+      ) as HTMLInputElement
+    ).value;
+    postBoard(accessToken, params.get("subject") ?? "", [
+      {
+        startFrom,
+        secondsFromStartToBeLate: +startToBeLate,
+        secondsFromBeLateToEnd: +beLateToEnd,
+      },
+    ]);
   }
 
   return (
