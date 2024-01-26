73ec53879a1e1669019495c3e996ef118d10ea44
MikuroXina <ryosukadnak@gmail.com>
2024-01-27 01:28:10 +0900

    Change to require changeAllAfter

--- src/commands/put-board.ts
+++ src/commands/put-board.ts
@@ -5,6 +5,7 @@ export async function putBoard(
   accessToken: string,
   subjectId: string,
   board: AttendanceBoards[number],
+  changeAllAfter: boolean,
 ) {
   const res = await fetch(
     `${API_ROOT}/subjects/${subjectId}/board/${board.id}`,
@@ -18,6 +19,7 @@ export async function putBoard(
         start_from: board.startFrom,
         seconds_from_start_to_be_late: board.secondsFromStartToBeLate,
         seconds_from_be_late_to_end: board.secondsFromBeLateToEnd,
+        change_all_after: changeAllAfter,
       }),
     },
   );
